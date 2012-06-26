c192ee751c20c0094469143d70d246557db1b62f
Bogdan Dobrica <bdobrica@gmail.com>
2012-06-26 23:16:55 +0200

    alternative SGBD: using also mysqli

--- main.php
+++ main.php
@@ -1,5 +1,7 @@
 <?php
 /* database */
+define ('CRYPTO_USE_SQLITE3', FALSE);
+if (defined(CRYPTO_USE_SQLITE3)) {
 class Crypto_DB extends SQLite3 {
 	private $Crypto_Q;
 	private $Crypto_R;
@@ -17,6 +19,27 @@ class Crypto_DB extends SQLite3 {
 			}
 		return $this->Crypto_R;
 		}
+	};
+	}
+else {
+class Crypto_DB extends mysqli {
+	private $Crypto_Q;
+	private $Crypto_R;
+
+	public function __construct ($name, $user = '', $pass = '') {
+		parent::__construct ('localhost', $user, $pass, $name);
+		}
+
+	public function q ($sql) {
+		$this->Crypto_Q = parent::query ($sql);
+		if ($this->Crypto_Q === FALSE) return -1;
+		$this->Crypto_R = array ();
+		while ($row = $this->Crypto_Q->fetch_array(MYSQLI_ASSOC)) {
+			$this->Crypto_R[] = $row;
+			}
+		return $this->Crypto_R;
+		}
+	};
 	}
 /* includes */
 include (dirname(__FILE__).'/libs/class_xtpl.php');
@@ -24,14 +47,17 @@ include (dirname(__FILE__).'/include/basic.php');
 include (dirname(__FILE__).'/include/aaa.php');
 /* define */
 define ('CRYPTO_PATH', dirname(__FILE__));
-define ('CRYPTO_URL', 'http://192.168.1.15');
+define ('CRYPTO_URL', 'http://t.xuri.ro');
 
 list ($uri, $urq) = explode ('?', trim($_SERVER['REQUEST_URI']));
 $uri = trim ($uri, '/');
 $urp = explode ('/', $uri);
 
 /* global */
-$dbu = new Crypto_DB ('/tmp/user');
+if (defined(CRYPTO_USE_SQLITE3))
+	$dbu = new Crypto_DB ('/tmp/user');
+else
+	$dbu = new Crypto_DB ('crypto', 'cryusr', 'crypwd');
 
 $t = new XTemplate (CRYPTO_PATH . '/template/un.html');
 /* session */
